2bed53c82d957c5fdb621e1781674cebea8180b5
hnawar <hnawar@google.com>
2018-12-20 20:58:50 +0000

    Adding a script that would combine the download and trimming reducing data transfer

---
+++ scripts/ftp_n_trimm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ftp_urls=$(curl "https://www.ebi.ac.uk/ena/data/warehouse/filereport?accession=${1}&result=read_run&fields=fastq_ftp" -s | sed -n 2p)
+
+url_list=$(echo $ftp_urls | tr ";" "\n")
+
+for addr in $url_list
+do
+	wget  $addr
+done
+cutadapt -a AGATCGGAAGAGC -A AGATCGGAAGAGC -o trimmed/${1}_1.t.fastq.gz -p trimmed/${1}_1.t.fastq.gz -O 5 --minimum-length=50 ${1}_1.fastq.gz ${1}_2.fastq.gz
+
