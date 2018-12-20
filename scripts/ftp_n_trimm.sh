42e5141ddf408a7c320abe7efc13f9733cedb7f4
Hatem <hnawar@google.com>
2018-12-20 22:16:24 +0000

    Update ftp_n_trimm.sh
    Adding set -x to better debug the error

--- scripts/ftp_n_trimm.sh
+++ scripts/ftp_n_trimm.sh
@@ -7,5 +7,7 @@ for addr in $url_list
 do
 	wget  $addr
 done
+set -x
+mkdir trimmed
 cutadapt -a AGATCGGAAGAGC -A AGATCGGAAGAGC -o trimmed/${1}_1.t.fastq.gz -p trimmed/${1}_1.t.fastq.gz -O 5 --minimum-length=50 ${1}_1.fastq.gz ${1}_2.fastq.gz
 
