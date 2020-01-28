af5a2420025859e2d83df9f65372a08e46d93f59
Hatem <hnawar@google.com>
2020-01-28 20:59:59 +0000

    Update ftp_n_trimm.sh
    Add https as this was failing on older versions of curl

--- scripts/ftp_n_trimm.sh
+++ scripts/ftp_n_trimm.sh
@@ -5,7 +5,7 @@ url_list=$(echo $ftp_urls | tr ";" "\n")
 
 for addr in $url_list
 do
-	curl  $addr -O -sS
+	curl  https://$addr -O -sS
 done
 set -x
 cutadapt -a AGATCGGAAGAGC -A AGATCGGAAGAGC -o ${2} -p ${3} -O 5 --minimum-length=50 ${1}_1.fastq.gz ${1}_2.fastq.gz
